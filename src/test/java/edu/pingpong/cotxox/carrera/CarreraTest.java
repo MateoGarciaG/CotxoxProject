fa3fbdc819d508007ccb5acd7ae0d068a4b9f743
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-02-17 12:53:19 +0100

    test(CarreraTest Class): :white_check_mark: new test cases: setDatosCarreraTest() that check if setters works

--- src/test/java/edu/pingpong/cotxox/carrera/CarreraTest.java
+++ src/test/java/edu/pingpong/cotxox/carrera/CarreraTest.java
@@ -36,15 +36,39 @@ public class CarreraTest {
         assertEquals(this.origen, this.carrera.getOrigen());
         assertEquals(this.destino, this.carrera.getDestino());
         assertEquals(this.distancia, this.carrera.getDistancia(), 0);
-        assertEquals(10.2d, this.carrera.getCosteEsperado(), 0);
+        assertEquals(10.2, this.carrera.getCosteEsperado(), 0);
         
 
-        System.out.println("Visa: " + carrera.getTarjetaCredito());
-        System.out.println("From: " + carrera.getOrigen());
-		System.out.println("To: " + carrera.getDestino());
-		System.out.println("Distance: " + carrera.getDistancia());
+        System.out.println("Visa: " + this.carrera.getTarjetaCredito());
+        System.out.println("From: " + this.carrera.getOrigen());
+		System.out.println("To: " + this.carrera.getDestino());
+		System.out.println("Distance: " + this.carrera.getDistancia());
         // Este método es responsabilidad de Tarifa por lo cual lo MOCKEARé por ahora
-        System.out.println("Total: " + carrera.getCosteEsperado() + "€");
+        System.out.println("Total: " + this.carrera.getCosteEsperado() + "€");
+    }
+
+    @Test
+    public void setDatosCarreraTest() {
+
+
+
+        this.carrera.setOrigen("Manacor");
+		this.carrera.setDestino("Magaluf");
+		this.carrera.setDistancia(11.52);
+		this.carrera.setTiempoEsperado(40);
+
+        assertEquals("Manacor", this.carrera.getOrigen());
+        assertEquals("Magaluf", this.carrera.getDestino());
+        assertEquals(11.52, this.carrera.getDistancia(), 0);
+        assertEquals(10.2, this.carrera.getCosteEsperado(), 0);
+        
+
+        System.out.println("Visa: " + this.carrera.getTarjetaCredito());
+        System.out.println("From: " + this.carrera.getOrigen());
+		System.out.println("To: " + this.carrera.getDestino());
+		System.out.println("Distance: " + this.carrera.getDistancia());
+        // Este método es responsabilidad de Tarifa por lo cual lo MOCKEARé por ahora
+        System.out.println("Total: " + this.carrera.getCosteEsperado() + "€");
     }
 
     

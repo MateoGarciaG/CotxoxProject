6729dcf1e3175c12b601c6554c88104462142c94
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-02-17 12:13:08 +0100

    feat(CarreraTest Class): :sparkles: Implementation getTarjetaCredito() method od Carrera is done

--- src/main/java/edu/pingpong/cotxox/carrera/Carrera.java
+++ src/main/java/edu/pingpong/cotxox/carrera/Carrera.java
@@ -2,5 +2,17 @@ package edu.pingpong.cotxox.carrera;
 
 public class Carrera {
     
-    
+    private String tarjetaCredito = "";
+
+
+    public Carrera(String tarjetaCredito) {
+        this.tarjetaCredito = tarjetaCredito;
+    }
+
+
+    public String getTarjetaCredito() {
+        return this.tarjetaCredito;
+    }
+
+
 }
